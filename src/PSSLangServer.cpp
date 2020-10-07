9a406b89725e876dc559281c7e264fa4cd533878
Matthew Ballance <matt.ballance@gmail.com>
2020-10-06 22:27:21 -0400

    Add copyrights...
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/PSSLangServer.cpp
+++ src/PSSLangServer.cpp
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 /*
  * PSSLangServer.cpp
  *
@@ -6,9 +24,13 @@
  */
 
 #include "PSSLangServer.h"
+#include "nlohmann/json.hpp"
 #include "TextDocumentSyncOptions.h"
+#include "Position.h"
+#include "ValInt.h"
+#include "ValStr.h"
 
-PSSLangServer::PSSLangServer() {
+PSSLangServer::PSSLangServer() : m_connection(0) {
 	// TODO Auto-generated constructor stub
 
 }
@@ -18,7 +40,9 @@ PSSLangServer::~PSSLangServer() {
 }
 
 lls::ServerCapabilitiesSP PSSLangServer::initialize(
-			lls::InitializeParamsSP params) {
+		lls::IClientConnection		*connection,
+		lls::InitializeParamsSP 	params) {
+	m_connection = connection;
 	lls::ServerCapabilitiesSP capabilities = lls::ServerCapabilities::mk();
 
 	lls::TextDocumentSyncOptionsSP textDocumentSync =
@@ -28,5 +52,49 @@ lls::ServerCapabilitiesSP PSSLangServer::initialize(
 	capabilities->textDocumentSync(textDocumentSync);
 
 	return capabilities;
+}
+
+void PSSLangServer::didChangeTextDocument(
+			lls::DidChangeTextDocumentParamsSP params) {
+	fprintf(stdout, "didChangeTextDocument: %s\n", params->textDocument()->uri()->val().c_str());
+	fflush(stdout);
+
+	lls::ValVector<lls::Diagnostic>::SP diagnostics =
+			lls::ValVector<lls::Diagnostic>::mk();
+	fprintf(stdout, "create diagnostics\n");
+	fflush(stdout);
+	diagnostics->push_back(lls::Diagnostic::mk(
+			lls::Range::mk(
+					lls::Position::mk(lls::ValInt::mk(4), lls::ValInt::mk(2)),
+					lls::Position::mk(lls::ValInt::mk(4), lls::ValInt::mk(8))),
+			lls::ValStr::mk(std::string("Error here"))));
+	fprintf(stdout, "create diagnostics (2)\n");
+	fflush(stdout);
+	lls::PublishDiagnosticsParamsSP diagnostic_p =
+			lls::PublishDiagnosticsParams::mk(
+					params->textDocument()->uri(),
+					diagnostics);
+
+	fprintf(stdout, "diagnostic_p: %s\n", diagnostic_p->dump().dump().c_str());
+
+	fprintf(stdout, "connection=%p\n", m_connection);
+	fflush(stdout);
+	m_connection->publishDiagnostics(diagnostic_p);
+	fprintf(stdout, "<-- didChangeTextDocument: %s\n", params->textDocument()->uri()->val().c_str());
+	fflush(stdout);
+}
+
+void PSSLangServer::didOpenTextDocument(
+			lls::DidOpenTextDocumentParamsSP params) {
+	fprintf(stdout, "--> didOpenTextDocument: %s\n",
+			params->textDocument()->uri()->val().c_str());
+	fflush(stdout);
+	lls::DocumentSP doc = m_docmgr.openDocument(
+			params->textDocument()->uri()->val(),
+			params->textDocument()->text()->val());
+	fprintf(stdout, "<-- didOpenTextDocument: %s\n",
+			params->textDocument()->uri()->val().c_str());
+	fflush(stdout);
+
 }
 
